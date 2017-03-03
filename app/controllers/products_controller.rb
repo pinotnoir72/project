b1ebdbedf4dc0756eb359dcd4ea73b1625e910c8
Nelda Hernandez <neldah72@gmail.com>
2017-03-03 06:26:52 +0000

    add and update a new controller for orders

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,7 @@ class ProductsController < ApplicationController
   # GET /products.json
   def index
     @products = Product.all
+   
   end
 
   # GET /products/1
--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,17 @@ Rails.application.routes.draw do
   get 'static_pages/contact'
 
   get 'static_pages/index'
+  
+  get 'static_pages/landing_page'
+
+resources :orders, only: [:index, :show, :create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-      root 'static_pages#index' 
+       #root 'static_pages#index'
+      root 'static_pages#landing_page'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
