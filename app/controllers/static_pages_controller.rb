8d187e8a6a5af1cbd9da3f236023d0b76579b9fc
Nelda Hernandez <neldah72@gmail.com>
2017-04-06 02:09:56 +0000

    4.5 Image Carousel in Views

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,7 +6,8 @@ class StaticPagesController < ApplicationController
   
 
  def landing_page
-    @featured_product = Product.first
+   @products = Product.limit(4)
+    
  end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,3 @@
-class Order < ApplicationRecord
-     belongs_to :product
+class Product < ActiveRecord::Base
+belongs_to :product
 end
