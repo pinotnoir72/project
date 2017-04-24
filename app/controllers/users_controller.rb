1b3274332615f2e7767a63fac43db64cf0bfab6e
Nelda Hernandez <neldah72@gmail.com>
2017-04-24 03:55:05 +0000

    5.4 devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, :except => [:show, :index].
+  before_action :authenticate_user!, :except => [:show, :index]
   before_filter :authenticate_user!
   load_and_authorize_resource
 
