c819fbd3e74a011d790f9068c6c95bb17f273c77
Nelda Hernandez <neldah72@gmail.com>
2017-04-30 01:55:20 +0000

    devise views

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,3 +1,6 @@
-$( document ).on('turbolinks:load', function() {
-  $(".alert, .notice").delay(4000).fadeOut(1000);
+$(document).on('turbolinks:load', function() {
+  $(".alert, .notice").delay(3000).fadeOut(1000);
 })
+
+
+
