eedd349369c893804ba99168d7529356ecc222d6
Nelda Hernandez <neldah72@gmail.com>
2017-05-03 03:27:23 +0000

    5.8 Comments and Raty

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,19 @@
 class Product < ActiveRecord::Base
     has_many :orders
     has_many :comments
+
+
+  def self.search(search_term)
+    Product.where("name LIKE ?", "%#{search_term}%")
+  end
+
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
+
+  def average_rating
+    comments.average(:rating).to_f
+  end
+
 end
 
-def self.search(search_term)
-  Product.where("name LIKE ?", "%#{search_term}%")
-end
-
-def highest_rating_comment
-  comments.rating_desc.first
-end
