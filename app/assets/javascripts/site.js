572a6ea7b11e57a0f39d7afb2cdcb26f048e83f4
Nelda Hernandez <neldah72@gmail.com>
2017-06-18 05:15:14 +0000

    6.10 AJAX

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,12 +1,16 @@
-$(document).on('turbolinks:load', function(){
+var refreshRating = function() {
     $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
     $('.rated').raty({ path: '/assets',
-      readOnly: true,
-      score: function() {
-        return $(this).attr('data-score');
-      }
+        readOnly: true,
+        score: function() {
+            return $(this).attr('data-score');
+        }
     });
-    
+};
+
+
+$(document).on('ready page:load' ajaxSuccess, function(){
+   
     $('.img-zoom').elevateZoom({
       zoomWindowFadeIn:700,
 			zoomWindowFadeOut: 700,
