a44f0dfa7f2e7ebc807c03a0e53ea85be012fdf0
Nelda Hernandez <neldah72@gmail.com>
2017-07-22 00:17:19 +0000

    revise product controller and static pages

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -38,6 +38,7 @@ class ProductsController < ApplicationController
   def create
     @product=Product.new(product_params)
       ProductChannel.broadcast_to @product.id, comment: CommentsController.render(partial: 'comments/comment', locals: {comment: @comment, current_user: current_user}), average_rating: @product.average_rating
+   
     respond_to do |format|
       if @product.save
         
